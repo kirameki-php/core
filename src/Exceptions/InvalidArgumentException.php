1612ce99e843abb421eb014d298c6749545d50b7
Taka Oyama <toyama4649@hotmail.com>
2023-02-01 23:50:30 +0900

    add InvalidArgumentException

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SouthPointe\Core\Exceptions;
+
+/**
+ * Thrown when an argument is not what the method expected.
+ */
+class InvalidArgumentException extends LogicException
+{
+}
